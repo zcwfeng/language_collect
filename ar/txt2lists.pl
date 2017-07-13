e93145245f0bd1429183d947a1c0125706e7629f
zcw <zcwfeng@126.com>
2017-07-13 20:14:18 +0800

    ar 阿拉伯语

---
+++ ar/txt2lists.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -CASD
+
+# Extracts Arabic words and outputs them line-delimited in
+# lexicographical order and as a frequency list into cwd
+# install deps with `cpan List::Uniq Lingua::AR::Tashkeel`
+
+use strict;
+use warnings;
+use List::Uniq 'uniq';
+use Lingua::AR::Tashkeel v0.004 'strip';
+use autodie ':all';
+
+local $/; $, = "\n";
+my ($content, $filename, @words, %freq, %stripped) = (<>, $ARGV[0] // 'stdin');
+@words = split ' ', $content =~ s/[\P{InArabic}\p{Punct}\d]+/ /gux;
+$freq{$stripped{$_} ||= strip $_}++ foreach @words;
+
+open my $fh, '>', "$filename.freqlist";
+foreach my $word (sort { $freq{$b} <=> $freq{$a} } keys %freq) {
+    print $fh "$word $freq{$word}\n";
+}
+open $fh, '>', "$filename.wordlist";
+print $fh grep { length > 1 } uniq {
+    sort => 1,
+    compare => sub { $stripped{$_[0]} cmp $stripped{$_[1]} }
+}, @words;
